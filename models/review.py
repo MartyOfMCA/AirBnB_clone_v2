d6d6e19e0a546828f184690bf8e30507f58cb53b
Nakuru Wubni <nwubni@gmail.com>
2023-09-20 19:37:47 +0100

    Gave a more descriptive doc string

--- models/review.py
+++ models/review.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review class to store review information """
+    """ A class to store reviews """
     place_id = ""
     user_id = ""
     text = ""
